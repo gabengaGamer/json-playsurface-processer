ac0a1c722666d061848e33f7cdf97bf645a9b7e5
GameSpy <a014969@gmail.com>
2025-01-31 02:42:30 +0100

    Quality of life update
    Improved mesh names in blender.

--- psurfjson.py
+++ psurfjson.py
@@ -5,15 +5,11 @@ import mathutils
 import math
 import re
 
-MODEL_DIRECTORY = r"C:\Users\GameSpy\Downloads\IneV\rigidgeom" # Put here your gltf models.
+MODEL_DIRECTORY = r"E:\InevitableProject\mp\2tango" # Put here your gltf models from DFSViewer.
 
-PLAYSURFACE_FILE_PATH = r"C:\Users\GameSpy\Downloads\IneV\data.json" # Put here exported playsurface .json from DFSViewer.
+PLAYSURFACE_FILE_PATH = r"E:\InevitableProject\mp\2tango.json" # Put here exported playsurface .json from DFSViewer.
 
-BIN_LEVEL_FILE_PATH = r"C:\Users\GameSpy\Downloads\IneV\data.json" # Put here exported bin_level .json from DFSViewer.
-
-imported_models_cache = {}
-
-## PLAYSURFACE CODE - START
+imported_models_cache = {} # Model cache allow speed up import by 20 times!
 
 def parse_playsurface_file(file_path):
     with open(file_path, "r") as file:
@@ -22,18 +18,18 @@ def parse_playsurface_file(file_path):
     models = []
     for zone in data.get("zones", []):
         for surface in zone.get("surfaces", []):
-            model = {
+            model_data = {
                 "geom_name": surface["geomName"].replace(".rigidgeom", ".gltf"),
                 "l2w": [surface["localToWorld"][i:i+4] for i in range(0, 16, 4)],
             }
-            models.append(model)
+            models.append(model_data)
     return models
 
 def create_blender_playsurface_matrix(l2w):
     blender_matrix = mathutils.Matrix(l2w).transposed()
 
     z_up_to_y_up = mathutils.Matrix.Rotation(math.pi / 2, 4, 'X')
-    rotate_minus_180_x = mathutils.Matrix.Rotation(math.radians(-180), 4, 'X')
+    rotate_minus_180_x = mathutils.Matrix.Rotation(math.radians(-180), 4, 'X') # For some reason objects have wrong rotation, i just adjust and as result fix it!
     corrected_matrix = blender_matrix @ z_up_to_y_up @ rotate_minus_180_x
 
     return corrected_matrix
@@ -84,19 +80,27 @@ def rename_and_deduplicate_playsurface_materials(obj, gltf_path):
                     else:
                         slot.material.name = texture_name
 
-def import_and_position_playsurface_model(model):
-    geom_path = os.path.join(MODEL_DIRECTORY, model["geom_name"])
+def import_and_position_playsurface_model(model_data):
+    geom_path = os.path.join(MODEL_DIRECTORY, model_data["geom_name"])
     if not os.path.exists(geom_path):
         return
 
-    l2w_matrix = create_blender_playsurface_matrix(model["l2w"])
+    l2w_matrix = create_blender_playsurface_matrix(model_data["l2w"])
+    base_name = os.path.splitext(os.path.basename(model_data["geom_name"]))[0]
+    name_counter = 0  
 
-    if model["geom_name"] in imported_models_cache:
-        imported_object = imported_models_cache[model["geom_name"]]
+    def get_unique_name(base_name):
+        nonlocal name_counter
+        while f"{base_name}_{name_counter:02d}" in bpy.data.objects:
+            name_counter += 1
+        return f"{base_name}_{name_counter:02d}"
+
+    if model_data["geom_name"] in imported_models_cache:
+        imported_object = imported_models_cache[model_data["geom_name"]]
         new_instance = imported_object.copy()
         new_instance.data = imported_object.data.copy()
+        new_instance.name = get_unique_name(base_name)
         bpy.context.collection.objects.link(new_instance)
-
         new_instance.matrix_world = l2w_matrix
     else:
         bpy.ops.import_scene.gltf(filepath=geom_path, merge_vertices=True)
@@ -114,33 +118,33 @@ def import_and_position_playsurface_model(model):
             bpy.ops.object.join()
 
             unified_object = meshes_to_join[0]
+            unified_object.name = get_unique_name(base_name)
             unified_object.matrix_world = l2w_matrix
             rename_and_deduplicate_playsurface_materials(unified_object, geom_path)
-            imported_models_cache[model["geom_name"]] = unified_object
+            imported_models_cache[model_data["geom_name"]] = unified_object
         else:
             for obj in imported_objects:
+                obj.name = get_unique_name(base_name)
                 obj.matrix_world = l2w_matrix
                 rename_and_deduplicate_playsurface_materials(obj, geom_path)
-                imported_models_cache[model["geom_name"]] = obj
-
-## PLAYSURFACE CODE - END
+                imported_models_cache[model_data["geom_name"]] = obj
 
 def rotate_entire_scene():
-    scene_rotation = mathutils.Matrix.Rotation(math.radians(90), 4, 'X')
+    mesh_rotation = mathutils.Matrix.Rotation(math.radians(90), 4, 'X')
     
     for obj in bpy.data.objects:
-        if obj.type in {'MESH', 'EMPTY'}:
-            obj.matrix_world = scene_rotation @ obj.matrix_world
+        if obj.type in {'MESH'}:
+            obj.matrix_world = mesh_rotation @ obj.matrix_world           
 
 def build_scene():
     models = parse_playsurface_file(PLAYSURFACE_FILE_PATH)
-
+    
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False)
-
-    for model in models:
-        import_and_position_playsurface_model(model)
-
+    
+    for model_data in models:
+        import_and_position_playsurface_model(model_data)
+                      
     rotate_entire_scene()
 
 build_scene()
